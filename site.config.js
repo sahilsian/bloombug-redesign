2730f0ff2e0778c1933584d5efb8d58310f55bff
sahilsian <sahilsian101@gmail.com>
2024-06-02 19:10:02 -0300

    .

---
+++ site.config.js
@@ -0,0 +1,38 @@
+const siteConfig = {
+    colors: {
+        solids: {
+            primary: '#BF4C93', // Primary color for buttons, links, etc.
+            secondary: '#FFFFFF', // Secondary color for accents, etc.
+            accent: '#BF4C93', // Accent color for highlights, etc.
+            error: '#dc3545', // Error color for validation messages, etc.
+            success: '#28a745', // Success color for success messages, etc.
+            cover: '#412842',
+            dark: '#412842',
+            danger: '#FF0000'
+        },
+        texts: {
+            primary: '#FFFFFF',
+            secondary: '#32353A',
+            danger: '#FFFFFF' 
+        }
+    },
+    texts: {
+        sitename: "Bloom Bug Screens",
+        formvalidation: "By opting into the web form above you are providing consent Bloom Bug Screens to contact you back for your inquiry.",
+        footer : {
+            footertext: "We are all female owned SME located in Surrey, BC specializing in top-quality bug screens, soft closed retractable screens, window blinds & roller shades, shower doors and in-home painting services. We use high-quality raw materials in all our projects.",
+            email: "hello@bloombug.ca",
+            phone: "(604) 722-4371",
+            location: "G-1100 Lansdowne Dr Coquitlam BC inside Vivid Tile & Stone",
+            location_dest: "https://search.app.goo.gl/ZGb5bSk",
+            instagram: "https://www.instagram.com/bloombug.ca",
+            facebook: "https://www.facebook.com/bloombugscreens/",
+            tiktok: "https://www.tiktok.com/@bloom.bug.screens",
+            cta: "Get a Quote with Bloom Bug Screens Today"
+        }
+        
+    }
+  };
+
+  
+  export default siteConfig;
