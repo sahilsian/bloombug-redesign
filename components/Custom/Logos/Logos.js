e4f9d7c77625e670ead31e53baba3793fff27f2f
sahilsian <sahilsian101@gmail.com>
2024-06-09 23:51:21 -0300

    feat: target_blank on logos

--- components/Custom/Logos/Logos.js
+++ components/Custom/Logos/Logos.js
@@ -9,7 +9,7 @@ export const Logos = ( { logo_1, logo_1_link, logo_2, logo_2_link, logo_3, logo_
             {
                 logo_1.url && (
                     <div className="min-w-[100px] max-w-[150px] flex justify-center">
-                        <Link href={logo_1_link || router.asPath}>
+                        <Link target="_blank" href ={logo_1_link || router.asPath}>
                         <Image src={logo_1.url} width={logo_1.width} height={logo_1.height} alt={logo_1.alt}></Image>
                         </Link>
 
@@ -19,7 +19,7 @@ export const Logos = ( { logo_1, logo_1_link, logo_2, logo_2_link, logo_3, logo_
             {
                 logo_2.url && (
                     <div className="min-w-[100px] max-w-[150px] flex justify-center">
-                        <Link href={logo_2_link || router.asPath}>
+                        <Link target="_blank" href ={logo_2_link || router.asPath}>
                         <Image src={logo_2.url} width={logo_2.width} height={logo_2.height} alt={logo_2.alt}></Image>
                         </Link>
                     </div>
@@ -28,7 +28,7 @@ export const Logos = ( { logo_1, logo_1_link, logo_2, logo_2_link, logo_3, logo_
             {
                 logo_3.url && (
                     <div className="min-w-[100px] max-w-[150px] flex justify-center">
-                        <Link href={logo_3_link || router.asPath}>
+                        <Link target="_blank" href ={logo_3_link || router.asPath}>
                         <Image src={logo_3.url} width={logo_3.width} height={logo_3.height} alt={logo_3.alt}></Image>
                         </Link>
                     </div>
@@ -37,7 +37,7 @@ export const Logos = ( { logo_1, logo_1_link, logo_2, logo_2_link, logo_3, logo_
             {
                 logo_4.url && (
                     <div className="min-w-[100px] max-w-[150px] flex justify-center">
-                        <Link href={logo_4_link || router.asPath}>
+                        <Link target="_blank" href ={logo_4_link || router.asPath}>
                         <Image src={logo_4.url} width={logo_4.width} height={logo_4.height} alt={logo_4.alt}></Image>
                         </Link>
                     </div>
@@ -46,7 +46,7 @@ export const Logos = ( { logo_1, logo_1_link, logo_2, logo_2_link, logo_3, logo_
             {
                 logo_5.url && (
                     <div className="min-w-[100px] max-w-[150px] flex justify-center">
-                        <Link href={logo_5_link || router.asPath}>
+                        <Link target="_blank" href ={logo_5_link || router.asPath}>
                         <Image src={logo_5.url} width={logo_5.width} height={logo_5.height} alt={logo_5.alt}></Image>
                         </Link>
                     </div>
@@ -55,7 +55,7 @@ export const Logos = ( { logo_1, logo_1_link, logo_2, logo_2_link, logo_3, logo_
             {
                 logo_6.url && (
                     <div className="min-w-[100px] max-w-[150px] flex justify-center">
-                        <Link href={logo_6_link || router.asPath}>
+                        <Link target="_blank" href ={logo_6_link || router.asPath}>
                         <Image src={logo_6.url} width={logo_6.width} height={logo_6.height} alt={logo_6.alt}></Image>
                         </Link>
                     </div>
